aa312dc00be293162f2b269446fc765f2bed983e
JainumSanghavi <sanghavi.h.j20@gmail.com>
2022-04-15 16:08:01 +0530

    Admin report generation complete

--- screens/adminReport.js
+++ screens/adminReport.js
@@ -12,8 +12,50 @@ import {
   TouchableWithoutFeedback,
   Keyboard,
 } from 'react-native';
+import firestore from '@react-native-firebase/firestore';
+
 
 const AdminReport = ({route, navigation}) => {
+
+  const accept = key => {
+    //write what happens after accept
+    firestore()
+      .collection('Bills')
+      .doc(key)
+      .update({
+        status: 'accepted',
+      })
+      .then(() => {
+        console.log('bill accepted');
+      });
+  };
+  const reject = key => {
+    //write what happens after reject
+    firestore()
+      .collection('Bills')
+      .doc(key)
+      .update({
+        status: 'rejected',
+      })
+      .then(() => {
+        console.log('bill rejected');
+      });
+  };
+  const edit = item => {
+    //write what happens after edit
+    firestore()
+      .collection('Bills')
+      .doc(item.key)
+      .update({
+        status: 'rejected',
+      })
+      .then(() => {
+        console.log('bill rejected');
+      });
+    navigation.navigate('Bill Proposal');
+  };
+
+
   const title = route.params.data.title;
   const preamble = route.params.data.preamble;
   const enac = route.params.data.enactingClause;
@@ -30,14 +72,70 @@ const AdminReport = ({route, navigation}) => {
   const key = route.params.data.key;
   const status = route.params.data.status;
   const arts = route.params.data.arts;
+  
+  const [up_percent, setup] = useState(0)
+  const [down_percent, setdown] = useState(0)
+
+  const articleIDs = arts
+  const [dat,setDat] = useState([])
+  useEffect(()=>{
+    const tot = upvotes+downvotes;
+    setup(((upvotes/tot)*100).toFixed(2))
+
+    var articles=[]
+    
+    articleIDs.forEach(pushFunction)
+    
+    function pushFunction(value, index, array){
+        console.log("dcoum ids, ",value)
+        
+        firestore()
+        .collection("Article")
+        .doc(value)
+        .onSnapshot(documentSnapshot=>{
+            console.log("document  ", documentSnapshot.data().title)
+            articles.push({
+                // 'title': documentSnapshot.data().title,
+                // 'key': documentSnapshot.id,
+                // "hashtags": documentSnapshot.data().hashtags,
+
+                key: documentSnapshot.id,
+                title: documentSnapshot.data().title,
+                data: documentSnapshot.data().data,
+                likes: documentSnapshot.data().likes,
+                dislikes: documentSnapshot.data().dislikes,
+                hashtags: documentSnapshot.data().hashtags,
+                uname: documentSnapshot.data().uname,
+            })
+            setDat(articles)
+
+        })
+        
+    
+    }
+    //setDat(articles)
+    
+    console.log("articles", dat)
+    
+    
+
+    
+  },[])
 
   return (
     <ScrollView style={styles.container}>
-      <Text> Admin page</Text>
-      <Text>{status}</Text>
-      <Text style={styles.title}>Title</Text>
-      <Text style={styles.content}>{title}</Text>
-      <Text style={styles.title}>preamble</Text>
+      {/* <Text> Admin page</Text>
+      <Text>{status}</Text> */}
+      <Text style={styles.title}>{title} {"\n"}</Text>
+      {/* <Text style={styles.content}>{title}</Text> */}
+      
+      <Text style={styles.title} >Total Upvotes: {upvotes}</Text>
+      <Text style={styles.title}>Total Downvotes: {downvotes} {"\n"}</Text>
+
+      <Text style={styles.title}>{up_percent}% for the motion</Text>
+      <Text style={styles.title}>{(100 - up_percent).toFixed(2)}% against the motion  {"\n"}</Text>
+
+      <Text style={styles.title}>Preamble</Text>
       <Text style={styles.content}>{preamble}</Text>
 
       <Text style={styles.title}>Enacting Clause</Text>
@@ -55,9 +153,43 @@ const AdminReport = ({route, navigation}) => {
       <Text style={styles.title}>summary</Text>
       <Text style={styles.content}>{summary}</Text>
 
+      
+
       <Text style={styles.title}>Cost: </Text>
       <Text style={styles.content}>{cost}</Text>
 
+      <Text style={styles.title}>View articles written on the bill </Text>
+      <FlatList
+        data={dat}
+        renderItem={({item}) => (
+          // return a component using that data
+          <TouchableOpacity
+
+          onPress={()=>
+          navigation.navigate("ArticleView",{ item: item, username: username, userId, userId})
+          }
+            // onPress={() =>
+            //   navigation.navigate('ViewBill', {
+            //     data: item,
+            //     username: username,
+            //     userId: userId,
+            //   })
+            >
+            <View style={styles.listitem}>
+              <View style={styles.topPart}>
+                <Text style={styles.title}>{item.title}</Text>
+
+                <Text style={styles.number} color="white">
+                 Hashtags: {item.hashtags}{' '}
+                </Text>
+              </View>
+              {/* <View style={styles.bottomCard}>
+                <Text>Comments</Text>
+              </View> */}
+            </View>
+          </TouchableOpacity>
+        )}
+      />
       {status === 'Active' && (
         <Button
           title="vote"
@@ -75,10 +207,22 @@ const AdminReport = ({route, navigation}) => {
           }
         />
       )}
-      <Button
-        title="Check Articles"
-        onPress={() => navigation.navigate('billArticles', {articleIDs: arts})}
-      />
+
+      <Text>{"\n"}{"\n"}</Text>
+       <Button
+                  title="Accept"
+                  color="green"
+                  onPress={() => accept(item.key)}
+                />
+                <Button
+                  title="Reject"
+                  color="red"
+                  onPress={() => reject(item.key)}
+                />
+
+            <Button title="Edit" color="grey" onPress={() => edit(item)} /> 
+
+      <Text>{"\n"}{"\n"}</Text>
     </ScrollView>
   );
 };
@@ -104,6 +248,96 @@ const styles = StyleSheet.create({
   content: {
     color: 'black',
     fontSize: 18,
+  },
+
+  // container: {
+  //   flex: 1,
+  //   justifyContent: 'center',
+  //   backgroundColor: 'white',
+  // },
+  listitem: {
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    borderRadius: 22,
+    marginTop: 25,
+    height: 160,
+    width: '100%',
+    color: 'white',
+    //marginLeft:20
+  },
+  text: {
+    fontWeight: 'bold',
+    color: 'black',
+    fontSize: 15,
+    opacity: 1,
+  },
+  // title: {
+  //   color: 'white',
+  //   fontSize: 22,
+  //   fontWeight: 'bold',
+  //   padding: 10,
+  // },
+  number: {
+    color: 'white',
+    fontSize: 18,
+    fontStyle: 'italic',
+    marginLeft: 10,
+  },
+  avatar: {
+    paddingLeft: 50,
+  },
+  appButtonContainer1: {
+    elevation: 8,
+    backgroundColor: 'grey',
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+    width: 170,
+    marginRight: 20,
+    marginLeft: 10,
+    marginTop: 20,
+  },
+  appButtonContainer2: {
+    elevation: 8,
+    backgroundColor: 'blue',
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+    width: 170,
+    marginRight: 20,
+    marginLeft: 10,
+    marginTop: 20,
+  },
+  endDate: {
+    position: 'absolute',
+    left: 275,
+    fontSize: 18,
+    top: 125,
+    color: 'white',
+  },
+  appButtonText: {
+    fontSize: 15,
+    color: '#fff',
+    fontWeight: 'bold',
+    alignSelf: 'center',
+    textTransform: 'uppercase',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+  },
+  topPart: {
+    backgroundColor: '#537A5A',
+    height: 160,
+    width: '100%',
+    // borderBottomLeftRadius: number,
+    // borderBottomRightRadius: number,
+    borderRadius: 20,
+    color: 'white',
+  },
+  bottomCard: {
+    flexDirection: 'row',
+    top: 350,
+    left: 200,
   },
 });
 export default AdminReport;
